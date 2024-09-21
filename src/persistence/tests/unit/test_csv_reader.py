5535043b082c4e23d7613a4cb493526dd157854d
Caio Martins <icaiomarxm@gmail.com>
2024-09-21 13:35:53 -0300

    tests: adding fixture and unit tests to csv_reader

---
+++ src/persistence/tests/unit/test_csv_reader.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+from src.persistence.csv_reader import parse_csv_to_model
+
+
+SAMPLE_CSV_PATH = "src/persistence/tests/fixtures/sample.csv"
+
+
+class ContentRow(BaseModel):
+    id: int
+    name: str
+    description: str
+
+
+def test_read_csv_with_model():
+    content = parse_csv_to_model(file_path=SAMPLE_CSV_PATH, target_entity=ContentRow)
+    for item in content:
+        assert isinstance(item, ContentRow)
+
+
+def test_read_csv_without_model():
+    content = parse_csv_to_model(file_path=SAMPLE_CSV_PATH)
+    for item in content:
+        assert isinstance(item, dict)
+        assert "id" in item.keys()
+        assert "name" in item.keys()
+        assert "description" in item.keys()
