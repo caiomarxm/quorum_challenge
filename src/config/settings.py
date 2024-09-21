b48601999db24536c77ea01356f9fb40c75e540d
Caio Martins <icaiomarxm@gmail.com>
2024-09-21 13:51:41 -0300

    feat: reading file paths from envs

---
+++ src/config/settings.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env.development", extra="ignore")
+
+    BILLS_CSV_FILE_PATH: str
+    LEGISLATORS_CSV_FILE_PATH: str
+    VOTES_FILE_PATH: str
+    VOTE_RESULTS_CSV_FILE_PATH: str
