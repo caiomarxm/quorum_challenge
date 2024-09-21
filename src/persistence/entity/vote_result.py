0682877c0f5cdb901764b8ec2486f3d6bcaec87c
Caio Martins <icaiomarxm@gmail.com>
2024-09-21 13:59:07 -0300

    feat: adding calculated field voted_yes

--- src/persistence/entity/vote_result.py
+++ src/persistence/entity/vote_result.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
+from typing import Literal
 
 
 class VoteResult(BaseModel):
     id: int
     legislator_id: int
     vote_id: int
-    vote_type: int
+    vote_type: Literal[1, 2]
+
+    @computed_field
+    @property
+    def voted_yes(self):
+        return self.vote_type == 1
