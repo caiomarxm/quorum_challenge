e19c6d31f60388d891d88b25d0b27c91a658af25
Caio Martins <icaiomarxm@gmail.com>
2024-09-21 14:05:50 -0300

    renaming target_entity to target_model in parse_csv_to_model params

--- src/persistence/repository/csv_reader.py
+++ src/persistence/repository/csv_reader.py
@@ -3,13 +3,13 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-def parse_csv_to_model(file_path: str, target_entity: Optional[BaseModel] = None):
+def parse_csv_to_model(file_path: str, target_model: Optional[BaseModel] = None):
     with open(file=file_path, mode="r") as file:
         content = csv.reader(file, delimiter=",")
         headers = next(content)
         for row in content:
             row_dict = dict(zip(headers, row))
-            if target_entity:
-                yield target_entity.model_validate(row_dict)
+            if target_model:
+                yield target_model.model_validate(row_dict)
             else:
                 yield row_dict
