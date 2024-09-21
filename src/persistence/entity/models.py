8911b7706522f67d02f29582952e9e338fb0cd7e
Caio Martins <icaiomarxm@gmail.com>
2024-09-21 14:50:20 -0300

    update: changing vte_type values from int to string and adding annotation in voted_yes

--- src/persistence/entity/models.py
+++ src/persistence/entity/models.py
@@ -22,9 +22,9 @@ class VoteResult(BaseModel):
     id: int
     legislator_id: int
     vote_id: int
-    vote_type: Literal[1, 2]
+    vote_type: Literal["1", "2"]
 
     @computed_field
     @property
-    def voted_yes(self):
-        return self.vote_type == 1
+    def voted_yes(self) -> bool:
+        return self.vote_type == "1"
