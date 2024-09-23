6880f544d4d143ecdba89b26cc8877a0dedc0d58
Caio Martins <icaiomarxm@gmail.com>
2024-09-23 09:42:53 -0300

    feat: including routers to views and api

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-from src.http.controller import router
+
+from src.api.controller import router
+from src.view import view
 
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 app.include_router(router=router, prefix="/api")
+app.include_router(router=view.router)
